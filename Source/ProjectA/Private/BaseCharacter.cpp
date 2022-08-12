a382ed92f51c5bffab91609e0e9b1bc59db7dae5
Enzo Tiezzi <enzo_tiezzi@hotmail.com>
2022-08-12 14:25:43 -0300

    2d project testing

---
+++ Source/ProjectA/Private/BaseCharacter.cpp
@@ -0,0 +1,74 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BaseCharacter.h"
+#include <GameFramework/CharacterMovementComponent.h>
+#include <PaperZD/Public/PaperZDAnimationComponent.h>
+#include <PaperZD/Public/PaperZDAnimInstance.h>
+
+ABaseCharacter::ABaseCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>("SpringArmComponent");
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>("CameraComponent");
+
+	SpringArmComponent->SetupAttachment(GetRootComponent());
+	CameraComponent->SetupAttachment(SpringArmComponent, USpringArmComponent::SocketName);
+
+	SpringArmComponent->SetRelativeRotation(FQuat(FRotator(0, -90, 0)));
+}
+
+void ABaseCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ABaseCharacter::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+}
+
+void ABaseCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	PlayerInputComponent->BindAxis("Move", this, &ABaseCharacter::Move);
+
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ABaseCharacter::StartJumping);
+	PlayerInputComponent->BindAction("Attack", IE_Pressed, this, &ABaseCharacter::Attack);
+
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+void ABaseCharacter::Move(float AxisValue)
+{
+	if (Controller && AxisValue != 0)
+	{
+		FVector WorldDirection = FVector::ForwardVector;
+
+		AddMovementInput(WorldDirection, AxisValue);
+	}
+}
+
+void ABaseCharacter::StartJumping()
+{
+	if (GetCharacterMovement()->IsMovingOnGround()) 
+	{
+		Jump();
+
+		GetAnimationComponent()->GetAnimInstance()->JumpToNode("Jump");
+	}
+}
+
+void ABaseCharacter::Attack() 
+{
+	if (!bIsAttacking)
+	{
+		bIsAttacking = true;
+
+		GetAnimationComponent()->GetAnimInstance()->JumpToNode("Attack1");
+	}
+}
+
+void ABaseCharacter::ResetCombat()
+{
+	bIsAttacking = false;
+}
---
+++ Source/ProjectA/Public/BaseCharacter.h
@@ -0,0 +1,56 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperZDCharacter.h"
+#include <GameFramework/SpringArmComponent.h>
+#include <Camera/CameraComponent.h>
+#include "BaseCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTA_API ABaseCharacter : public APaperZDCharacter
+{
+	GENERATED_BODY()
+	
+public:
+	ABaseCharacter();
+
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaSeconds) override;
+
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	USpringArmComponent* SpringArmComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	UCameraComponent* CameraComponent;
+
+	///////////////////////////////////////////////////
+	///
+	/// MOVEMENT 
+	/// 
+	///////////////////////////////////////////////////
+	void Move(float AxisValue);
+
+	void StartJumping();
+
+	///////////////////////////////////////////////////
+	///
+	/// COMBAT 
+	/// 
+	///////////////////////////////////////////////////
+	void Attack();
+
+	UFUNCTION(BlueprintCallable)
+	void ResetCombat();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Combat")
+	bool bIsAttacking = false;
+};
